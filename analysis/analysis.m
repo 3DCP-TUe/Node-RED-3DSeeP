ebad24ef1727b0e41087eb96fee0b1708a2efaa1
arjendeetman <arjendeetman@hotmail.com>
2024-06-26 11:52:07 +0200

    Update analysis.m

--- analysis/analysis.m
+++ analysis/analysis.m
@@ -15,7 +15,12 @@ path = mfilename('fullpath');
 cd(filepath);
 
 %% Import data from node red
-nodeRed = readtable('20240607_AlexIdil.csv');
+% One file
+%nodeRed = readtable('20240614_IdilAlex.csv');
+% For combining multiple files
+nodeRed1 = readtable('20240617_RILEM_DAY1_1.csv');
+nodeRed2 = readtable('20240617_RILEM_DAY1_2.csv');
+nodeRed = [nodeRed1; nodeRed2];
 
 %% Get time in minutes and seconds
 nodeRed.('seconds') = seconds(nodeRed.('desktop_time')) - seconds(nodeRed.('desktop_time')(1));
@@ -24,15 +29,20 @@ nodeRed.('minutes') = minutes(nodeRed.('desktop_time')) - minutes(nodeRed.('desk
 %% Calculations
 % Differential pressure
 k = 60; % ~6 seconds, 10 samples per second
-filterd1 = movmean(nodeRed.('material_io_ai0_pressure_bar'), k, 'omitnan');
-filterd2 = movmean(nodeRed.('material_io_ai1_pressure_bar'), k, 'omitnan');
-nodeRed.('material_differential_pressure_bar') = filterd1 - filterd2;
+filtered1 = movmean(nodeRed.('material_io_ai0_pressure_bar'), k, 'omitnan');
+filtered2 = movmean(nodeRed.('material_io_ai1_pressure_bar'), k, 'omitnan');
+nodeRed.('material_differential_pressure_bar') = filtered1 - filtered2;
+% Filtered values from coriolis io
+nodeRed.('material_coriolis_mass_flow_filtered_90s_kg_min') = (nodeRed.('material_io_ai4_ma') - 4) / 16 * 16;
+nodeRed.('material_coriolis_density_filtered_90s_kg_m3') = (nodeRed.('material_io_ai5_ma') - 4) / 16 * 400 + 2000;
 % Mixer timing
 [times, intervalTimes, runTimes] = mixerTimes(nodeRed.('minutes'), nodeRed.('mai_mixer_run_bool'));
 % Printhead pressure
 if ~any(strcmp(nodeRed.Properties.VariableNames, 'printhead_pressure_bar'))
   nodeRed.('printhead_pressure_bar') = (nodeRed.('printhead_box1_io_ai0_ma') - 4) / 16 * 10;
 end
+% Temperature pumping chamber MAI MULTIMIX
+nodeRed.('mai_temperature_pumping_chamber_c') = (nodeRed.('material_io_ai2_ma') - 4) / 16 * 100;
 
 %% Plot pressure
 fig = figure;
@@ -46,14 +56,14 @@ plot(nodeRed.('minutes'), nodeRed.('material_io_ai0_pressure_bar'), '.k', 'Marke
 plot(nodeRed.('minutes'), nodeRed.('material_io_ai1_pressure_bar'), '.b', 'MarkerSize', 2)
 % Limits
 ylim([0 25])
-xlim([0 240])
+xlim([0 360])
 % Labels
 xlabel('Time [Minutes]')
 ylabel('Pressure [bar]')
 % Legend
 legend('Pressure sensor 1', 'Pressure sensor 2', 'Location', 'NorthEast')
 % Layout
-set(gca,'XTick',(0:15:1000))
+set(gca,'XTick',(0:30:900))
 % Write figure
 saveFigure(fig, 'pressure')
 
@@ -70,10 +80,10 @@ yyaxis right
 plot(nodeRed.('minutes'), nodeRed.('printhead_pressure_bar'), '.b', 'MarkerSize', 2)
 % Limits
 yyaxis left
-ylim([0 1.2])
+ylim([0 1.4])
 yyaxis right
-ylim([0 1.2])
-xlim([0 240])
+ylim([0 1.4])
+xlim([0 360])
 % Labels
 xlabel('Time [Minutes]')
 yyaxis left
@@ -83,7 +93,7 @@ ylabel('Differential pressure [bar]')
 % Legend
 legend('Differential pressure coriolis', 'Differential pressure printhead', 'Location', 'NorthEast')
 % Layout
-set(gca,'XTick',(0:15:1000))
+set(gca,'XTick',(0:30:900))
 yyaxis left
 set(gca, 'YColor','k')
 yyaxis right
@@ -102,12 +112,12 @@ box on
 plot(nodeRed.('minutes'), nodeRed.('material_coriolis_dynamic_viscocity_cp'), '.k', 'MarkerSize', 2)
 % Limits
 ylim([0 6000])
-xlim([0 240])
+xlim([0 360])
 % Labels
 xlabel('Time [Minutes]')
 ylabel('Apparent dynamic viscocity [cP]')
 % Layout
-set(gca,'XTick',(0:15:1000))
+set(gca,'XTick',(0:30:900))
 % Write figure
 saveFigure(fig, 'viscocity')
 
@@ -122,14 +132,37 @@ box on
 plot(nodeRed.('minutes'), nodeRed.('material_coriolis_exciter_current_1_ma'), '.k', 'MarkerSize', 2)
 % Limits
 ylim([0 10])
-xlim([0 240])
+xlim([0 360])
 % Labels
 xlabel('Time [Minutes]')
 ylabel('Exciter current 1 [mA]')
 % Layout
-set(gca,'XTick',(0:15:1000))
+set(gca,'XTick',(0:30:900))
 % Write figure
 saveFigure(fig, 'exciter_current_1')
+
+%% Plot mass flow rate
+fig = figure;
+fig.Units = 'centimeters';
+fig.Position = [1 14 24 8];
+hold on
+grid on
+box on
+% Plot data
+plot(nodeRed.('minutes'), nodeRed.('material_coriolis_mass_flow_kg_min'), '.k', 'MarkerSize', 2)
+plot(nodeRed.('minutes'), nodeRed.('material_coriolis_mass_flow_filtered_90s_kg_min'), '.b', 'MarkerSize', 2)
+% Limits
+ylim([0 12])
+xlim([0 360])
+% Labels
+xlabel('Time [Minutes]')
+ylabel('Mass flow rate [kg/min]')
+% Legend
+legend('Unfiltered', 'Filter E+H 90s', 'Location', 'NorthEast')
+% Layout
+set(gca,'XTick',(0:30:900))
+% Write figure
+saveFigure(fig, 'mass_flow')
 
 %% Plot temperature
 fig = figure;
@@ -140,14 +173,17 @@ grid on
 box on
 % Plot data
 plot(nodeRed.('minutes'), nodeRed.('material_coriolis_temperature_c'), '.k', 'MarkerSize', 2)
+plot(nodeRed.('minutes'), nodeRed.('mai_temperature_pumping_chamber_c'), '.b', 'MarkerSize', 2)
 % Limits
 ylim([26 36])
-xlim([0 240])
+xlim([0 360])
 % Labels
 xlabel('Time [Minutes]')
 ylabel('Mortar temperature [C]')
+% Legend
+legend('Coriolis sensor', 'Pumping chamber', 'Location', 'NorthEast')
 % Layout
-set(gca,'XTick',(0:15:1000))
+set(gca,'XTick',(0:30:900))
 % Write figure
 saveFigure(fig, 'mortar_temperature')
 
@@ -160,14 +196,17 @@ grid on
 box on
 % Plot data
 plot(nodeRed.('minutes'), nodeRed.('material_coriolis_density_kg_m3'), '.k', 'MarkerSize', 2)
+plot(nodeRed.('minutes'), nodeRed.('material_coriolis_density_filtered_90s_kg_m3'), '.b', 'MarkerSize', 2)
 % Limits
 ylim([2320 2400])
-xlim([0 240])
+xlim([0 360])
 % Labels
 xlabel('Time [Minutes]')
 ylabel('Density [kg/m^{3}]')
+% Legend
+legend('Unfiltered', 'Filter E+H 90s', 'Location', 'NorthEast')
 % Layout
-set(gca,'XTick',(0:15:1000))
+set(gca,'XTick',(0:30:900))
 % Write figure
 saveFigure(fig, 'density')
 
@@ -182,12 +221,12 @@ box on
 plot(nodeRed.('minutes'), nodeRed.('mai_pump_speed_chz')./100, '.k', 'MarkerSize', 2)
 % Limits
 ylim([0 50])
-xlim([0 240])
+xlim([0 360])
 % Labels
 xlabel('Time [Minutes]')
 ylabel('Pump frequency [Hz]')
 % Layout
-set(gca,'XTick',(0:15:1000))
+set(gca,'XTick',(0:30:900))
 % Write figure
 saveFigure(fig, 'mortar_pump_frequency')
 
@@ -202,12 +241,12 @@ box on
 plot(nodeRed.('minutes'), nodeRed.('mai_pump_output_power_w'), '.k', 'MarkerSize', 2)
 % Limits
 ylim([0 800])
-xlim([0 240])
+xlim([0 360])
 % Labels
 xlabel('Time [Minutes]')
 ylabel('Pump output power [W]')
 % Layout
-set(gca,'XTick',(0:15:1000))
+set(gca,'XTick',(0:30:900))
 % Write figure
 saveFigure(fig, 'mortar_pump_output_power')
 
@@ -222,12 +261,12 @@ box on
 plot(nodeRed.('minutes'), nodeRed.('mai_water_temp_c'), '.k', 'MarkerSize', 2)
 % Limits
 ylim([15 21])
-xlim([0 240])
+xlim([0 360])
 % Labels
 xlabel('Time [Minutes]')
 ylabel('Water temperature [C]')
 % Layout
-set(gca,'XTick',(0:15:1000))
+set(gca,'XTick',(0:30:900))
 % Write figure
 saveFigure(fig, 'water_temperature')
 
@@ -243,14 +282,14 @@ plot(nodeRed.('minutes'), nodeRed.('mai_water_flow_actual_lh'), '.k', 'MarkerSiz
 plot(nodeRed.('minutes'), nodeRed.('mai_water_flow_set_lh'), '.b', 'MarkerSize', 2)
 % Limits
 ylim([160 220])
-xlim([0 240])
+xlim([0 360])
 % Labels
 xlabel('Time [Minutes]')
 ylabel('Water flow [L/h]')
 % Legend
 legend('Actual', 'Setpoint', 'Location', 'NorthEast')
 % Layout
-set(gca,'XTick',(0:15:1000))
+set(gca,'XTick',(0:30:900))
 % Write figure
 saveFigure(fig, 'water_flow')
 
@@ -266,14 +305,14 @@ plot(nodeRed.('minutes'), nodeRed.('mai_waterpump_output_freq_chz')./100, '.k', 
 plot(nodeRed.('minutes'), nodeRed.('mai_waterpump_ref_freq_chz')./100, '.b', 'MarkerSize', 2)
 % Limits
 ylim([0 30])
-xlim([0 240])
+xlim([0 360])
 % Labels
 xlabel('Time [Minutes]')
 ylabel('Water pump freq. [Hz]')
 % Legend
 legend('Actual', 'Reference', 'Location', 'NorthEast')
 % Layout
-set(gca,'XTick',(0:15:1000))
+set(gca,'XTick',(0:30:900))
 % Write figure
 saveFigure(fig, 'water_pump_frequency')
 
@@ -293,7 +332,7 @@ yyaxis left
 ylim([0 40])
 yyaxis right
 ylim([0 100])
-xlim([0 180])
+xlim([0 360])
 % Labels
 xlabel('Time [Minutes]')
 yyaxis left
@@ -303,7 +342,7 @@ ylabel('Relative humidity [%]')
 % Legend
 legend('Ambient temperature', 'Relative humidity', 'Location', 'NorthEast')
 % Layout
-set(gca,'XTick',(0:15:1000))
+set(gca,'XTick',(0:30:900))
 yyaxis left
 set(gca, 'YColor','k')
 yyaxis right
@@ -324,14 +363,14 @@ plot(times, runTimes./intervalTimes, '.k', 'MarkerSize', 2, 'LineWidth', 1.5)
 plot(times, movmean(runTimes./intervalTimes, [k 0]), '-k', 'MarkerSize', 2, 'LineWidth', 1.5)
 % Limits
 ylim([0 0.4])
-xlim([0 240])
+xlim([0 360])
 % Labels
 xlabel('Time [Minutes]')
 ylabel('Run time / interval time')
 % Legend
 legend('Single run', sprintf('Moving mean k=%d', k), 'Location', 'NorthEast')
 % Layout
-set(gca,'XTick',(0:15:1000))
+set(gca,'XTick',(0:30:900))
 % Write figure
 saveFigure(fig, 'mixer_times_ratio')
 
@@ -350,16 +389,39 @@ plot(times, runTimes*60, '.b', 'MarkerSize', 2)
 plot(times, movmean(runTimes*60, [k 0]), '-b', 'MarkerSize', 2, 'LineWidth', 1.5)
 % Limits
 ylim([0 120])
-xlim([0 240])
+xlim([0 360])
 % Labels
 xlabel('Time [Minutes]')
 ylabel('Mixer times [Seconds]')
 % Legend
 legend('Interval time', sprintf('Interval time mov. mean k=%d', k), 'Run time', sprintf('Run time mov. mean k=%d', k), 'Location', 'NorthEast')
 % Layout
-set(gca,'XTick',(0:15:1000))
+set(gca,'XTick',(0:30:900))
 % Write figure
 saveFigure(fig, 'mixer_times')
+
+%% Correlation between temperature and differential pressure
+fig = figure;
+fig.Units = 'centimeters';
+fig.Position = [1 14 11 8];
+hold on
+grid on
+box on
+% Window
+windowStart = 60; % Minutes 
+windowEnd = 270; % Minutes
+[~, index1] = min(abs(nodeRed.('minutes')-windowStart));
+[~, index2] = min(abs(nodeRed.('minutes')-windowEnd));
+% Plot data
+plot(nodeRed.('material_coriolis_temperature_c')(index1:index2), nodeRed.('material_differential_pressure_bar')(index1:index2), '.k', 'MarkerSize', 2)
+% Limits
+ylim([0 1.4])
+xlim([30 36])
+% Labels
+xlabel('Mortar temperature [C]')
+ylabel('Differential pressure [bar]')
+% Write figure
+saveFigure(fig, 'correlation_temp_dp')
 
 %% End
 disp('End of script')
