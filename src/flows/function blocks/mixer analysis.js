cc857d2a15b18289703aa7c666c6156815fcee80
arjendeetman <arjendeetman@hotmail.com>
2024-08-13 11:30:54 +0200

    MAI and m-tec mixer pump: Added the mean mixer time ratio of the last 10 cycles to the dashboard

---
+++ src/flows/function blocks/mixer analysis.js
@@ -0,0 +1,48 @@
+// On Start
+context.set('prev_mixer_run', false);
+context.set('prev_mixer_stop', new Date().getTime() / 1000);
+context.set('prev_mixer_start', new Date().getTime() / 1000);
+context.set('batch_time', 0.0);
+context.set('interval_time', 0.0);
+context.set('queue', []);
+
+// On Message
+if (msg.payload !== context.get('prev_mixer_run')) {
+   
+    context.set('prev_mixer_run', msg.payload);
+	
+	time = new Date().getTime() / 1000;
+	prev_start = context.get('prev_mixer_start')
+	prev_stop = context.get('prev_mixer_stop')
+	prev_batch_time = context.get('batch_time');
+	prev_interval_time = context.get('interval_time');
+	
+	if (msg.payload == false)
+	{
+	    batch_time = time - prev_start;
+		context.set('prev_mixer_stop', time);
+		context.set('batch_time', batch_time);
+	}
+	else
+	{
+		interval_time = time - prev_start;
+	    msg.batch_time = prev_batch_time;
+		msg.interval_time = interval_time;
+		msg.prediction = prev_batch_time / interval_time * 1.0;
+		
+		queue = context.get('queue');
+		queue.push(msg.prediction);
+		if(queue.length > 10)
+		{
+		    queue.shift();
+		}
+		
+		let sum = queue.reduce((acc, curr) => acc + curr, 0);
+		msg.mean = sum / queue.length;
+		
+		context.set('queue', queue);
+		context.set('interval_time', interval_time);
+		context.set('prev_mixer_start', time);
+		return msg;
+	}
+}
